33dbd4a1f71b0e7526805ea30ee83d92892082a0
altom7 <129540224+altom7@users.noreply.github.com>
2023-05-17 00:05:51 -0400

    final code: benchmark_checker.py

--- benchmark_checker.py
+++ benchmark_checker.py
@@ -16,7 +16,9 @@ Next, you can write 'python3 benchmark_checker.py' if you have a Mac device or '
 We did not require any outside sources to develop the program, other than knowledge from the class and previous classes, as well as the INST 326 class slides.
 """
 
+import io
 import unittest
+from unittest.mock import patch
 
 class Checker:
     """ A class to check which benchmarks the user has and has not completed according to which courses they have taken.
@@ -25,16 +27,15 @@ class Checker:
     def __init__(self):
         """ Initializes a Checker object with benchmark data read in from text files.
 
-        Args:
-            benchmark_files (list): a list of file names containing benchmark data
-        """
+    Args:
+        benchmark_files (list): a list of file names containing benchmark data
+    """
         self.benchmark_files = ['benchmark1.txt', 'benchmark2.txt', 'benchmark3.txt', 'benchmark4.txt']
         self.benchmarks = {}
         for file in self.benchmark_files:
-            with open(file, 'r') as f:
-                benchmark_name = file.split('.')[0]
+            with open(file, 'r', encoding='utf-8-sig') as f:  # add the encoding parameter
+                benchmark_name = file.rstrip('.txt')  # Splits the filename at the period
                 self.benchmarks[benchmark_name] = f.read().splitlines()
-
 
 
     def completed_courses(self):
@@ -68,19 +69,17 @@ class Checker:
 
 
     def get_completed_benchmarks(self, completed_courses):
-        """ Returns a list of completed benchmarks based on the completed courses entered by the user.
-
-        Args:
-            completed_courses (list): a list of course names completed by the user
-
-        Returns:
-            completed_courses (list): a list of course names completed by the user
-        """
         completed_benchmarks = []
         for benchmark_name, benchmark_courses in self.benchmarks.items():
+            print(f"Checking benchmark {benchmark_name}...")
+            print(f"Benchmark courses: {benchmark_courses}")
+            print(f"Completed courses: {completed_courses}")
             if self.check_benchmark_completion(benchmark_courses, completed_courses):
                 completed_benchmarks.append(benchmark_name)
+                print(f"Added {benchmark_name} to completed benchmarks.")
         return completed_benchmarks
+
+
 
 
 
@@ -149,3 +148,56 @@ class CourseProgress:
         for course in self.incomplete_courses:
             print(course)
 
+
+
+class TestChecker(unittest.TestCase):
+    """ A class to perform unit tests for the Checker class.
+    """
+    def setUp(self):
+        self.checker = Checker()
+
+    def test_check_benchmark_completion(self):
+        completed_courses = ['PSYC 100', 'MATH 115', 'STAT 100', 'INST 126', 'INST 201']
+        benchmark_courses = ['PSYC 100', 'MATH 115']  # Courses in benchmark1
+        self.assertTrue(self.checker.check_benchmark_completion(benchmark_courses, completed_courses))
+
+        benchmark_courses = ['STAT 100', 'INST 126', 'INST 201']  # Courses in benchmark2
+        self.assertTrue(self.checker.check_benchmark_completion(benchmark_courses, completed_courses))
+
+        benchmark_courses = ['INST 311', 'INST 314', 'INST 326', 'INST 327', 'INST 335', 'INST 346', 'INST 352', 'INST 362']  # Courses in benchmark3
+        self.assertFalse(self.checker.check_benchmark_completion(benchmark_courses, completed_courses))
+
+        benchmark_courses = ['INST 490']  # Courses in benchmark4
+        self.assertFalse(self.checker.check_benchmark_completion(benchmark_courses, completed_courses))
+
+    # Assume that the benchmark1 and benchmark2 lists contain the respective courses
+    def test_get_completed_benchmarks(self):
+        completed_courses = ['INST 201', 'INST 126', 'STAT 100', 'PSYC 100', 'MATH 115']
+        completed_benchmarks = self.checker.get_completed_benchmarks(completed_courses)
+        self.assertListEqual(completed_benchmarks, ['benchmark1', 'benchmark2'])
+
+# This test assumes that the benchmark1 to benchmark4 lists contain the respective courses
+def test_get_completed_benchmarks(self):
+    completed_courses = ['INST 201', 'INST 126', 'STAT 100', 'PSYC 100', 'MATH 115']
+    completed_benchmarks = self.checker.get_completed_benchmarks(completed_courses)
+    print("Expected: ['benchmark1', 'benchmark2']")
+    print(f"Actual: {completed_benchmarks}")
+    self.assertListEqual(completed_benchmarks, ['benchmark1', 'benchmark2'])
+
+
+class TestCourseProgress(unittest.TestCase):
+    """ A class to perform unit tests for the CourseProgress class.
+    """
+    def test_print_remaining_courses(self):
+        completed_courses = ['PSYC 100', 'MATH 115', 'INST 201', 'INST 126', 'STAT 100', 'INST 335', 'INST 326', 'INST 327', 'INST 314', 'INST 311', 'INST 362', 'INST 346', 'INST 352']
+        incomplete_courses = ['INST 490']
+        course_progress = CourseProgress(completed_courses, incomplete_courses)
+
+        expected_output = "Remaining courses to complete:\nINST 490\n"
+        with patch('sys.stdout', new=io.StringIO()) as fake_stdout:
+            course_progress.print_remaining_courses()
+        self.assertEqual(fake_stdout.getvalue(), expected_output)
+
+
+if __name__ == '__main__':
+    unittest.main()
